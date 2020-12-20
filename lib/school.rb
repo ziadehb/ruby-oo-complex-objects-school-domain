81744dffac3e05b98a37e60452679edb8ab5df0e
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-19 21:59:56 -0500

    Add files via upload

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School 
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name=name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        roster[grade]||= []
+        roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        self.roster.each {|grade, students| students.sort!}
+    end
+end
